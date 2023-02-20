9c5d73256abd8629f54268b1812dc5cafe25b98a
Ian Slade Ashivende <iashivende@gmail.com>
2023-02-20 14:24:04 -0800

    mf

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the Alphabet .
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp [26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+
