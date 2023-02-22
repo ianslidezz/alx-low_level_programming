9a212e8d7ce93ebdf31fde8339367f09309b301b
Ian Slade Ashivende <iashivende@gmail.com>
2023-02-21 17:36:14 -0800

    ki

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - prints 10 times the alphabet, in lowercase
+ * followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
