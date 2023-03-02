d9ecfcb140a4fd3428046692c25ab7deca325e0d
Ian Slade Ashivende <iashivende@gmail.com>
2023-03-01 23:10:08 -0800

    ewr

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,31 +1,31 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenates two strings.
- * null byte, to the end of string pointed to by @dest.
+ * _strcat - Conactenates two strings.
+ *
  * @dest: input value.
  * @src: input value.
  *
- * Return: void
+ * Return: Void.
  */
-char *_strcat(char *dest, const char *src)
+
+char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int k;
+	int n;
 
-	i = 0;
-	while (dest[i] != '\0')
+	k = 0;
+	while ((dest[k] = '\0'))
 	{
-		i++;
+		k++;
 	}
-	j = 0;
-	while (src[j] != '\0')
+	n = 0;
+	while ((src[n] = '\0'))
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+		dest[k] = src[n];
+		k++;
+		n++;
 	}
-
-	dest[i] = '\0';
+	dest[k] = '\0';
 	return (dest);
 }
