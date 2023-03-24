6644fff40c662ee60bb56bfce8595678305ac9d1
Ian Slade Ashivende <iashivende@gmail.com>
2023-03-24 13:37:28 -0700

    main - prints the name of the file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,10 +3,10 @@
 /**
  * main - prints the name of the file
  *
- * Return: Always zero (Sucess)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
 	return (0);
 }
