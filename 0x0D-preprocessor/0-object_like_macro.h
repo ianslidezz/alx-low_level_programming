ba2123d8aab23533fcc5b4b6050ea987878bf4ac
Ian Slade Ashivende <iashivende@gmail.com>
2023-03-24 06:53:16 -0700

    header file that defines a macro named SIZE

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -4,16 +4,5 @@
 #define SIZE 1024
 
 #include <stdio.h>
-/**
- * defines a macro named SIZE
- * prints Hello world
- *
- * returns 0
- */
 
-int main(void)
-{
-	printf("Hello, World!\n");
-	return (0);
-}
 #endif
