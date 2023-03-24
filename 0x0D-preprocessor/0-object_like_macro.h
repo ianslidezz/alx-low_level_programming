ad944d476b2f1992165a5cf6b898a8d213ea03e3
Ian Slade Ashivende <iashivende@gmail.com>
2023-03-24 06:50:46 -0700

    size

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -3,4 +3,17 @@
 
 #define SIZE 1024
 
+#include <stdio.h>
+/**
+ * defines a macro named SIZE
+ * prints Hello world
+ *
+ * returns 0
+ */
+
+int main(void)
+{
+	printf("Hello, World!\n");
+	return (0);
+}
 #endif
